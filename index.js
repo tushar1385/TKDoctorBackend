3d9fc153fccbe885c6c78fa0ad26b48d2fd116c8
Tushar Krishna <tusharwfl@gmail.com>
2025-09-28 23:14:57 +0530

    Add two more apis

--- index.js
+++ index.js
@@ -34,4 +34,39 @@ app.post("/api/doctors", async (req, res) => {
   }
 });
 
+// Update a data
+app.put("/api/doctors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedDoctor = await Doctor.findByIdAndUpdate(
+      id, 
+      req.body, 
+      { 
+        new: true, 
+        runValidators: true 
+      }
+    );
+    if (!updatedDoctor) {
+      return res.status(404).json({ error: "Doctor not found" });
+    }
+    res.json(updatedDoctor);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Delete data
+app.delete("/api/doctors/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedDoctor = await Doctor.findByIdAndDelete(id);
+    if (!deletedDoctor) {
+      return res.status(404).json({ error: "Doctor not found" });
+    }
+    res.status(200).json({ message: "Doctor deleted successfully", deletedDoctor });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- models/Doctor.js
+++ models/Doctor.js
@@ -3,7 +3,8 @@ import mongoose from "../dbConfig/mongo.js";
 const doctorSchema = new mongoose.Schema({
     drName: { type: String, required: true },
     drCode: { type: String },
-    mobile: { type: String },
+    mobile: { type: Number },
+    ffrmslno: { type: Number },
     email: { type: String },
     pan: { type: String },
     address: { type: String },
